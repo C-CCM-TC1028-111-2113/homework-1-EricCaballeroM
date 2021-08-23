9959dc597a52882d09dcc9cf673e38546e906c47
EricCaballeroM <88851026+EricCaballeroM@users.noreply.github.com>
2021-08-23 18:46:03 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+
+new=int(input("give me the total of new games: "))
+used=int(input("give me the total of used games: "))
+
+tnew=new*1000
+tused=used*350
+
+print("The total is ", tused+tnew)
+
    pass
 
 
