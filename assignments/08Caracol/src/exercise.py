0d194d9fe8e03f7180b0324fadc4218a7b96a0df
EricCaballeroM <88851026+EricCaballeroM@users.noreply.github.com>
2021-08-23 18:12:14 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    a=float(input("give me the minutes: "))
+
+s=a*60
+cm=s*5.7
+vf= cm/10
+print("traveled centimeters: ", vf)
     pass
 
 if __name__ == '__main__':
