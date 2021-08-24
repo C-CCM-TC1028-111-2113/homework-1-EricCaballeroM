0e7188fb3db3959da2b7f7b3df53ea8c75cba165
EricCaballeroM <88851026+EricCaballeroM@users.noreply.github.com>
2021-08-23 21:17:56 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,5 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
+    #publicación en una editorial
+def numpag(p):
+    b=p/475
+
+
+
+e=int(input("give the number of words: "))
+a=e%2
+t=numpag(e)
+x=((numpag(e)+1)*60)*0.90
+y=((numpag(e))*60)*0.90
+
+
+if a!=0:
+    numpag(e)+1
+    print("the publication cost is: ", x)
+else:
+    print("the publication cost is: ", y)
     pass
 
 
